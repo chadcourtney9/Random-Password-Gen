de5aa30b22c001d848e38c46c70e0be0c1e4c89d
Chad Courtney <Chadcourtney567@gmail.com>
2020-12-30 18:33:16 -0500

    called htmlelemnts, renaming id's. added eventlist

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
-
+const LowerCaseBoxEl = document.getElementById('LowerCaseBox')
+const UpperCaseBoxEl = document.getElementById('UpperCaseBox')
+const NumericBoxEl = document.getElementById('NumericBox')
+const SCBoxEl = document.getElementById('SCBox')
 
 const userSelect = {
     lowercase: getLowerCase,
@@ -6,18 +9,43 @@ const userSelect = {
     numbers: getNumericChar,
     symbols: getSpecialChar,
 }
-
+//giving constants to slider and number
 const userLength = document.getElementById
     ('charAmountNumber')
 const userSlider = document.getElementById
     ('charAmountSlider')
 
+//linking slider and number range
 userLength.addEventListener('input', syncCharacterAmount)
 userSlider.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const characterAmount = userLength.value
+    const UpperCaseBox = LowerCaseBoxEl.checked
+    const NumericBox = NumericBoxEl.checked
+    const SCBox = SCBoxEl.checked
+    const password = generatePassword(character)
+})
 
 function syncCharacterAmount(e) {
     const value = e.target.value
     userLength.value = value
+    userSlider.value = value
+}
+
+function generatePassword(UpperCaseBox, NumericBox, SCBox) {
+    let charCodes = getLowerCase
+    if (SCBox) charCodes = charCodes.concat(getSpecialChar)
+    if (UpperCaseBox) charCodes = charCodes.concat(getUpperCase)
+    if (NumericBox) charCodes = charCodes.concat(getNumericChar)
+
+    const passwordChars = []
+    for (let = 0; i < userLength; i++) {
+        const character = charCodes[Math.floor(Math.random() * charCodes.length)]
+        passwordChars.push(String.fromCharCode(charCodes))
+    }
+    return passwordChars.join('')
 }
 
 //97-122 ascii
@@ -61,8 +89,3 @@ function getCharFromRange(max, min) {
 console.log(
     getSpecialChar()
 )
-
-
-// if 
-// call a string
-// loop concat string
